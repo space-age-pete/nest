47259acefe8a6cc68df45bebf21efa3f527a7da7
pete <josh.furlin@gmail.com>
2020-01-05 20:02:38 -0600

    messed around with svg patterns, made mostly working catan shuffler

--- nest-client/src/App.js
+++ nest-client/src/App.js
@@ -1,27 +1,72 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import sheepHex from "./image2vector.svg";
+import HexBackSVG from "./components/HexBackSVG"
 
 function App() {
+  let colors = ["red", "orange", "yellow", "green", "blue", "yellow", "green", "red", "orange", "yellow", "green", "blue", "blue", "yellow", "green", "red"]
+  let [tileList, setTileList] = useState(["lightgreen", "lightgreen", "lightgreen", "lightgreen", "green", "green", "green", "green", "yellow", "yellow", "yellow", "yellow", "grey", "grey", "grey", "red", "red", "red", "tan"]);
+  let [selectedTile, setSelectedTile] = useState(null);
+  let [numberList, setNumberList] = useState([2,3,3,4,4,5,5,6,6,8,8,9,9,10,10,11,11,12]);
 
-  let color = event =>{
-    event.target.setAttribute("fill", "green")
-  }
+  console.log(HexBackSVG)
 
-  let uncolor = event =>{
-    event.target.setAttribute("fill", "purple")
-  }
+  let tile = 0;
+  let number = 0;
 
-  let row = ["red", "orange", "yellow", "green", "blue"]
   let grid= [
+    ["red", "orange", "yellow"],
+    ["green", "blue","red", "orange"],
     ["red", "orange", "yellow", "green", "blue"],
-    ["green", "blue","red", "orange", "yellow"],
-    ["red", "orange", "yellow", "green", "blue"]
+    ["green", "blue","red", "orange"],
+    ["red", "orange", "yellow"],
   ]
 
-  let arr = ["booger", "caravan"];
-  let h = arr.pop();
-  console.log(h, arr)
+  let shuffle = (b) => {
+    if(selectedTile){
+      selectedTile.setAttribute("stroke-width", 1);
+      setSelectedTile(null);
+    }
+    tile = 0;
+    let a = [...b]
+    for (let i = a.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [a[i], a[j]] = [a[j], a[i]];
+    }
+    setTileList(a)
+  }
+
+  let shuffle2 = (b) => {
+    if(selectedTile){
+      selectedTile.setAttribute("stroke-width", 1);
+      setSelectedTile(null);
+    }
+    number = 0;
+    let a = [...b]
+    for (let i = a.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [a[i], a[j]] = [a[j], a[i]];
+    }
+    setNumberList(a)
+  }
+
+  let swap = event => {
+    let clickedOn = event.target;
+    if(!selectedTile){
+      // clickedOn.setAttribute("stroke", "black");
+      clickedOn.setAttribute("stroke-width", "5")
+      
+      setSelectedTile(clickedOn);
+    } else {
+      let fillTemp = selectedTile.getAttribute("fill");
+      selectedTile.setAttribute("fill", clickedOn.getAttribute("fill"));
+      clickedOn.setAttribute("fill", fillTemp);
+      //selectedTile.removeAttribute("stroke");
+      selectedTile.setAttribute("stroke-width", 1);
+      setSelectedTile(null);
+    }
+  }
 
   return (
     // <div className="App">
@@ -40,37 +85,49 @@ function App() {
     //     </a>
     //   </header>
     // </div>
-    <svg viewBox="-110 -200 3150 1200" xmlns="http://www.w3.org/2000/svg">
+   <div> 
+     <button onClick={_=>shuffle(tileList)}>Shuffle Tiles</button>
+     <button onClick={_=>shuffle2(numberList)}>Shuffle Numbers</button>
+     <img src={sheepHex} className="App-logo" alt="logo" />
+     <svg viewBox="-110 -200 3150 1200" xmlns="http://www.w3.org/2000/svg">
       <g> 
         {/* <polygon onMouseEnter={color} onMouseLeave={uncolor} points="100,0 50,-87 -50,-87 -100,-0 -50,87 50,87" fill="purple" stroke="black" />
        <g transform="translate(0,179)"> <polygon  onMouseEnter={color} onMouseLeave={uncolor} points="100,0 50,-87 -50,-87 -100,-0 -50,87 50,87" fill="purple" stroke="black" /></g> */}
       {
         grid.map((row, index) => row.map((filler, i) => 
-        (<g key={i} transform={`translate(${150 * index},${174* i + (index%2 !== 0 ? 87 : 0)})`}> 
+        (<g key={i} transform={`translate(${150 * index},${174* i + (index%2 !== 0 ? 87 : ((index === 0 || index === 4)? 174 : 0))})`}> 
             <polygon 
               id={`hex-${index}-${i}`}
-              class={`hex-${index}-${i}`}
+              className={tileList[tile]}
               points="100,0 50,-87 -50,-87 -100,-0 -50,87 50,87"
-              fill={filler}
-              // stroke="black"
-            />
-            <text>H</text>
+              fill={tileList[tile++]}
+              stroke="black"
+              strokeWidth="1"
+              onClick={swap}
+              />
+            {/* <text>{index}, {i}</text> */}
+            <text>{tileList[tile - 1] === "tan" ? "D" : numberList[number++]}</text>
         </g>)
       ))
-      }
+    }
       
-      {/* {row.map((filler, i) => 
-        (<g key={i} transform={`translate(0,${175* i})`}> 
+      {/* <g transform={`translate(500,500)`}> 
+        
+      <HexBackSVG></HexBackSVG>
             <polygon 
+              id="armpit"
+              className="sheep"
               points="100,0 50,-87 -50,-87 -100,-0 -50,87 50,87"
-              fill={filler}
-              stroke="black"
-              onMouseEnter={color}
+              // fill="pink"
+              // stroke="black"
+              pattern="url(#svgg)"
+              onClick={swap}
             />
-        </g>)
-      )} */}
+            <text>sheep?</text>
+        </g> */}
       </g>
     </svg>
+    </div>
     // <div>
     //   {[<h1>Booger</h1>,[<h1>Booger</h1>,<h1>horses</h1>]]}
     // </div>
